16f9069dc403456e08fbf25a80408cda8bc7701e
William Ye <w3ye@pm.me>
2021-08-10 16:02:40 -0400

    added some comments and unimportant changes to previous commit

--- public/scripts/composer-char-count.js
+++ public/scripts/composer-char-count.js
@@ -3,7 +3,9 @@ $(document).ready(() => {
   const $tweetText = $('#tweet-text');
   $tweetText.keyup(function() {
     let difference = MAX_CHARACTERS - this.value.length;
+    // traverse up the DOM tree by one parent and finding .counter
     const $counter = $(this).parent().find('.counter');
+    // add CSS negative class when the user exceed 140 limit
     if (difference < 0) $counter.addClass('negative');
     else $counter.removeClass('negative');
     $counter.text(difference);
