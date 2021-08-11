0cbe4fc36fdbdd81940a99bb936e8a741a91e597
William Ye <w3ye@pm.me>
2021-08-11 17:31:43 -0400

    implemented createTweetElement

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,32 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweets = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1628449812239
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1628536212239
-  }
-];
-
-const tweet = {
+const tweetData = {
   "user": {
     "name": "Newton",
     "avatars": "https://i.imgur.com/73hZDYK.png",
@@ -39,3 +14,30 @@ const tweet = {
   },
   "created_at": 1628449812239
 };
+
+const createTweetElement = (tweet) => {
+  const tweetContainer =
+	`<article>
+	  <header>
+	    <div class="user-name"><img src="${tweet.user.avatars}"> <span>${tweet.user.name}</span></div>
+	    <div class="handle">${tweet.user.handle}</div>
+	  </header>
+	  <div class="content">${tweet.content.text}</div>
+	  <footer>
+	    <div>${timeago.format(tweet.created_at)}</div>
+	    <div class="icons">
+	      <i class="fas fa-flag"></i>
+	      <i class="fas fa-retweet"></i>
+	      <i class="fas fa-heart"></i>
+	    </div>
+	  </footer>
+	</article>`;
+  return tweetContainer;
+};
+
+const $tweet = createTweetElement(tweetData);
+
+$(document).ready(() => {
+  console.log($tweet);
+  $('#tweets-container').append($tweet);
+});
