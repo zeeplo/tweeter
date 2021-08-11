57b75aefd10e90d16461cc15238cc17aaef9d61d
William Ye <w3ye@pm.me>
2021-08-11 18:09:50 -0400

    serialize the data and prevents submit form's default actions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,17 +3,38 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1628449812239
+
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+];
+
+const renderTweets = (tweets) => {
+  for (let i of tweets) {
+    $('#tweets-container').append(createTweetElement(i));
+  }
 };
+
 
 const createTweetElement = (tweet) => {
   const tweetContainer =
@@ -35,9 +56,11 @@ const createTweetElement = (tweet) => {
   return tweetContainer;
 };
 
-const $tweet = createTweetElement(tweetData);
 
 $(document).ready(() => {
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
+  renderTweets(data);
+  $('#new-tweet-form').submit(function(event) {
+    event.preventDefault();
+    const serializedContent = $(this).serialize();
+  });
 });
