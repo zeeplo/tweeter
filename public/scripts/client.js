3e3f3a38681a8167ddbdf8fb487810aa4b636a8b
Benny <w3ye@pm.me>
2021-08-16 01:49:35 -0400

    empty the text area when a tweet is posted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,9 +10,9 @@
  * @param {Array} tweets
  */
 const renderTweets = (tweets) => {
-  $('#tweets-container').empty(); // Need to empty the section everytime it renders, otherwise every refresh the content will just append to old content instead of making new content
+  $("#tweets-container").empty(); // Need to empty the section everytime it renders, otherwise every refresh the content will just append to old content instead of making new content
   for (let i of tweets) {
-    $('#tweets-container').append(createTweetElement(i));
+    $("#tweets-container").append(createTweetElement(i));
   }
 };
 
@@ -22,10 +22,11 @@ const renderTweets = (tweets) => {
  * @returns {string}
  */
 const createTweetElement = (tweet) => {
-  const tweetContainer =
-	`<article>
+  const tweetContainer = `<article>
 	  <header>
-	    <div class="user-name"><img src="${tweet.user.avatars}"> <span>${tweet.user.name}</span></div>
+	    <div class="user-name"><img src="${tweet.user.avatars}"> <span>${
+    tweet.user.name
+  }</span></div>
 	    <div class="handle">${tweet.user.handle}</div>
 	  </header>
 	  <div class="content">${escape(tweet.content.text)}</div>
@@ -45,42 +46,39 @@ const createTweetElement = (tweet) => {
  * Loads the tweet from the database
  */
 const loadtweets = () => {
-  $.get('/tweets/')
-    .done((data) => {
-      renderTweets(data.reverse());
-    });
+  $.get("/tweets/").done((data) => {
+    renderTweets(data.reverse());
+  });
 };
-
 
 /**
  * Post the tweet into the database
  * If there is nothing in the tweet or the tweet is over 140 characters, the tweet will not go through
  */
 const postTweet = () => {
-  $('#new-tweet-form').submit(function(event) {
-
+  $("#new-tweet-form").submit(function (event) {
     event.preventDefault();
 
     // input validation
-    if ($('#tweet-text').val().length === 0) {
-      $('#input-err').html(function() {
-        $(this).slideDown('slow');
+    if ($("#tweet-text").val().length === 0) {
+      $("#input-err").html(function () {
+        $(this).slideDown("slow");
         return '<p><i class="fas fa-times"></i>    You can\'t tweet nothing</p>';
       });
-    } else if ($('#tweet-text').val().length > 140) {
-      $('#input-err').html(function() {
-        $(this).slideDown('slow');
+    } else if ($("#tweet-text").val().length > 140) {
+      $("#input-err").html(function () {
+        $(this).slideDown("slow");
         return '<p><i class="fas fa-times"></i>    You are over the 140 limit</p>';
       });
     } else {
       //error message will slide up if the tweet is valid
-      $('#input-err').slideUp('fast');
-      $('#input-err').empty();
+      $("#input-err").slideUp("fast");
+      $("#input-err").empty();
 
-      $.post('/tweets/', $(this).serialize())
-        .done(() => {
-          loadtweets();
-        });
+      $.post("/tweets/", $(this).serialize()).done(() => {
+        loadtweets();
+        $('#tweet-text').val('');
+      });
     }
   });
 };
@@ -98,8 +96,8 @@ const escape = (str) => {
  * Toggle new-tweets to show and hide
  */
 const toggleNewTweets = () => {
-  $('#write-new-tweet').click(function() {
-    $('.new-tweet').slideToggle("slow");
+  $("#write-new-tweet").click(function () {
+    $(".new-tweet").slideToggle("slow");
   });
 };
 
@@ -107,22 +105,22 @@ const toggleNewTweets = () => {
  * go-top hides on default, when the window is scrolled the button appears. It dissapears again after 5 seconds of not scrolling
  */
 const goTopScroll = () => {
-  $('#go-top').hide();
-  $(window).scroll(function() {
-    $('#go-top').show().delay(5000).fadeOut();
-  })
+  $("#go-top").hide();
+  $(window).scroll(function () {
+    $("#go-top").show().delay(5000).fadeOut();
+  });
 };
 
 /**
  * When the go-top link is clicked, it will scroll to the top and make sure that .new-tweet shows
  */
 const goTop = () => {
-  $('#go-top').click(function() {
-    $('.new-tweet').show();
+  $("#go-top").click(function () {
+    $(".new-tweet").show();
   });
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
   loadtweets();
   postTweet();
   toggleNewTweets();
